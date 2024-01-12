59582314044a4f0ef07c74510002454d1f7b624f
yellowamit <mail2amikg@gmail.com>
2024-01-12 20:45:48 +0530

    changes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
-const images = ["pic1.jpg", `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 const alts = {
   "pic1.jpg": "Closeup of a human eye",
   "pic2.jpg": "Rock that looks like a wave",
